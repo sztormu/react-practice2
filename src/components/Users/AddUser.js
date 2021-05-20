f5974ae2b6bd27b4a4ddd1f68bd19c8a89cbd357
Kacper Dobosz <68871709+sztormu@users.noreply.github.com>
2021-05-20 14:41:49 +0200

    Add Wrapper

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -5,6 +5,7 @@ import styles from './AddUser.module.css';
 import Card from '../UI/Card'
 import Button from '../UI/Button'
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
 
 
 const AddUser = props => {
@@ -57,7 +58,7 @@ const AddUser = props => {
 
 
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal
                 title={error.title}
                 message={error.message}
@@ -72,7 +73,7 @@ const AddUser = props => {
                     <Button type='submit' >Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
 
     )
 
